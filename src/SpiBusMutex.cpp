211c39ab449c7558ad417baf861d391fbbef2bd4
Unintendedsideeffects <malcolmcosenza@gmail.com>
2026-01-29 16:21:15 +0000

    fix: add null check for SPI mutex creation

--- src/SpiBusMutex.cpp
+++ src/SpiBusMutex.cpp
@@ -1,7 +1,14 @@
 #include "SpiBusMutex.h"
 
+#include <Arduino.h>
+
 SemaphoreHandle_t SpiBusMutex::getMutex() {
   static SemaphoreHandle_t mutex = xSemaphoreCreateMutex();
+  if (mutex == NULL) {
+    Serial.println("FATAL: Failed to create SPI bus mutex");
+    delay(1000);
+    ESP.restart();
+  }
   return mutex;
 }
 
