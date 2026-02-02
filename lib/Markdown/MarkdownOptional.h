b46661ba776b461d517fcca0f23e8a79d011b604
Unintendedsideeffects <malcolmcosenza@gmail.com>
2026-02-02 10:09:29 +0000

    generated files

---
+++ lib/Markdown/MarkdownOptional.h
@@ -0,0 +1,23 @@
+#pragma once
+
+template <typename T>
+class MdOptional {
+ public:
+  MdOptional() : hasValue(false), value_() {}
+  MdOptional(const T& value) : hasValue(true), value_(value) {}
+
+  MdOptional& operator=(const T& value) {
+    hasValue = true;
+    value_ = value;
+    return *this;
+  }
+
+  bool has_value() const { return hasValue; }
+  const T& value() const { return value_; }
+  T& value() { return value_; }
+  explicit operator bool() const { return hasValue; }
+
+ private:
+  bool hasValue;
+  T value_;
+};
